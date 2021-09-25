3727ad11585eb9b38764eb22b354c47d91fa9295
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2021-09-25 02:26:17 -0400

    Update Entrepot.java

--- src/model/Entrepot.java
+++ src/model/Entrepot.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class Entrepot extends Usine{
     private int capacite;
 
-    Entrepot(int id, TypeComposant type, Icone icone, Point position, Chemin chemin, int capacite) {
+    Entrepot(int id, TypeUsine type, Icone icone, Point position, Chemin chemin, int capacite) {
         super(id, type, icone, position, chemin);
         this.capacite = capacite;
     }
