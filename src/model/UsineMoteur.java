b25af5c96dae1f8b67032a8da863c3b1687ef8d9
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2021-09-25 02:25:27 -0400

    Update UsineMoteur.java

--- src/model/UsineMoteur.java
+++ src/model/UsineMoteur.java
@@ -3,7 +3,7 @@ package model;
 import java.awt.*;
 
 public class UsineMoteur extends UsineProduction{
-    UsineMoteur(int id, TypeComposant type, Icone icone, Point position, Chemin chemin, int quantite, int intervalleProduction) {
+    UsineMoteur(int id, TypeUsine type, Icone icone, Point position, Chemin chemin, int quantite, int intervalleProduction) {
         super(id, type, icone, position, chemin, quantite, intervalleProduction);
     }
 }
