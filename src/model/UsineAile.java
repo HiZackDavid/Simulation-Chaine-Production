cc11f4839eed5595adacb7573206fca3d84da266
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2021-09-25 02:25:30 -0400

    Update UsineAile.java

--- src/model/UsineAile.java
+++ src/model/UsineAile.java
@@ -3,7 +3,7 @@ package model;
 import java.awt.Point;
 
 public class UsineAile extends UsineProduction {
-    UsineAile(int id, TypeComposant type, Icone icone, Point position, Chemin chemin, int quantite, int intervalleProduction) {
+    UsineAile(int id, TypeUsine type, Icone icone, Point position, Chemin chemin, int quantite, int intervalleProduction) {
         super(id, type, icone, position, chemin, quantite, intervalleProduction);
     }
 }
