333f0c09a1c4632a10a6e653cf36928c1208784c
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2021-09-26 05:05:05 -0400

    Update TypeIcone.java
    Added a value converter from Enum to String

--- src/model/TypeIcone.java
+++ src/model/TypeIcone.java
@@ -2,4 +2,14 @@ package model;
 
 public enum TypeIcone {
     VIDE, UN_TIERS, DEUX_TIERS, PLEIN;
+
+    public static String getType(TypeIcone type) {
+        switch (type) {
+            case VIDE :  return "vide";
+            case UN_TIERS : return "un-tiers";
+            case DEUX_TIERS : return "deux-tiers";
+            case PLEIN : return "plein";
+            default : return null;
+        }
+    }
 }
