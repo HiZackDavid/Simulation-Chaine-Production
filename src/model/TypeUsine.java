edd5305cb8c9fab700815edeeca1d341b342ec55
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2021-09-25 22:30:54 -0400

    Update TypeUsine.java
    Added method that converts string values from the configuration file into TypeUsine values

--- src/model/TypeUsine.java
+++ src/model/TypeUsine.java
@@ -2,4 +2,21 @@ package model;
 
 public enum TypeUsine {
     ASSEMBLAGE, AILE, MOTEUR, MATIERE, ENTREPOT;
+
+    public static TypeUsine getType(String type) {
+        switch (type) {
+            case "usine-matiere" :
+                return TypeUsine.MATIERE;
+            case "usine-aile" :
+                return TypeUsine.AILE;
+            case "usine-moteur" :
+                return TypeUsine.MOTEUR;
+            case "usine-assemblage" :
+                return TypeUsine.ASSEMBLAGE;
+            case "entrepot" :
+                return TypeUsine.ENTREPOT;
+            default:
+                return null;
+        }
+    }
 }
