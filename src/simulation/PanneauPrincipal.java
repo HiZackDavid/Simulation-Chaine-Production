1e4c5cf2bd93f05bf6b2d07c7e4d249051e11608
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2021-09-26 05:55:38 -0400

    Update PanneauPrincipal.java
    Drawn the "Usines" and "Chemins"

--- src/simulation/PanneauPrincipal.java
+++ src/simulation/PanneauPrincipal.java
@@ -1,9 +1,12 @@
 package simulation;
 
-import java.awt.Graphics;
-import java.awt.Point;
+import controller.Configuration;
+import model.Chemin;
+import model.Usine;
 
-import javax.swing.JPanel;
+import java.awt.*;
+
+import javax.swing.*;
 
 public class PanneauPrincipal extends JPanel {
 
@@ -13,13 +16,57 @@ public class PanneauPrincipal extends JPanel {
 	private Point position = new Point(0,0);
 	private Point vitesse = new Point(1,1);
 	private int taille = 32;
-	
+
+	public static final int USINE_WIDTH = 32;
+	public static final int USINE_HEIGTH = 30;
+
 	@Override
 	public void paint(Graphics g) {
+		Configuration configuration = new Configuration("src/ressources/configuration.xml");
 		super.paint(g);
 		// On ajoute à la position le delta x et y de la vitesse
+		/*
 		position.translate(vitesse.x, vitesse.y);
 		g.fillRect(position.x, position.y, taille, taille);
-	}
+		*/
 
+		// Dessiner chemins
+		for (Usine usine : configuration.getUsines()) {
+			int x = usine.getPosition().x;
+			int y = usine.getPosition().y;
+
+			for (Chemin chemin : usine.getChemins()) {
+				Usine destination = null;
+
+				for (Usine usineDestination : configuration.getUsines()) {
+					if (usineDestination.getId() == chemin.getDestination()) {
+						destination = usineDestination;
+					}
+				}
+
+				int xDestination = destination.getPosition().x;
+				int yDestination = destination.getPosition().y;
+
+				g.drawLine(x, y, xDestination, yDestination);
+			}
+		}
+
+		// Dessiner usines
+		for (Usine usine : configuration.getUsines()) {
+			String iconPath = usine.getIcone().getPath();
+			int x = usine.getPosition().x;
+			int y = usine.getPosition().y;
+
+			// Rectangles temp
+			//g.fillRect(x - USINE_WIDTH/2, y - USINE_HEIGTH/2, USINE_WIDTH, USINE_HEIGTH);
+
+			// Dessiner Icones
+			ImageIcon imageIcon = new ImageIcon(iconPath);
+			Image image = imageIcon.getImage();
+			Image resizedImage = image.getScaledInstance(USINE_WIDTH, USINE_HEIGTH, Image.SCALE_SMOOTH);
+			ImageIcon icon = new ImageIcon(resizedImage);
+
+			icon.paintIcon(this, g, x - USINE_WIDTH/2, y - USINE_HEIGTH/2);
+		}
+	}
 }
