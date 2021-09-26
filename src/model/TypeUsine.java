2499ebe620d1bef1f3655a7b023dda0a963618ef
HiZackDavid <87014651+HiZackDavid@users.noreply.github.com>
2021-09-26 05:05:50 -0400

    Update TypeUsine.java
    Added a value converter from Enum to String.
    Re-ordered some line of codes

--- src/model/TypeUsine.java
+++ src/model/TypeUsine.java
@@ -5,16 +5,28 @@ public enum TypeUsine {
 
     public static TypeUsine getType(String type) {
         switch (type) {
-            case "usine-matiere" :
-                return TypeUsine.MATIERE;
+            case "usine-assemblage" :
+                return TypeUsine.ASSEMBLAGE;
             case "usine-aile" :
                 return TypeUsine.AILE;
             case "usine-moteur" :
                 return TypeUsine.MOTEUR;
-            case "usine-assemblage" :
-                return TypeUsine.ASSEMBLAGE;
+            case "usine-matiere" :
+                return TypeUsine.MATIERE;
             case "entrepot" :
                 return TypeUsine.ENTREPOT;
+            default:
+                return null;
+        }
+    }
+
+    public static String getType(TypeUsine type) {
+        switch (type) {
+            case ASSEMBLAGE: return "usine-assemblage";
+            case AILE: return "usine-aile";
+            case MOTEUR: return "usine-aile";
+            case MATIERE: return "usine-matiere";
+            case ENTREPOT: return "entrepot";
             default:
                 return null;
         }
